395aa294a620d4be3be20993fcdb97952f6fac24
Souza600 <eduardinhoreserva@gmail.com>
2026-01-11 15:11:26 -0300

    Fix: Corrigir erro de build na Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  safelist: [
-    'font-sans',
-    'font-serif',
-    'font-mono',
-    'antialiased',
-    'bg-[var(--color-bg)]',
-    'text-[var(--color-text)]'
-  ],
   theme: {
     extend: {},
-    fontFamily: {
-      sans: ['ui-sans-serif', 'system-ui', 'sans-serif'],
-      serif: ['ui-serif', 'Georgia', 'serif'],
-      mono: ['ui-monospace', 'SFMono-Regular', 'monospace'],
-    }
   },
   plugins: [],
-}
+};
